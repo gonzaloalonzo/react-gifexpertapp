856da238bdc2a526df0f1d28cb180d8e47cb175f
gonzaloalonzo <gonzaloalonzo@hotmail.com>
2020-07-02 15:34:51 -0400

    docs agregados

---
+++ docs/precache-manifest.2534d3b59596d6ce58c9228addb33898.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a1ce27db199e1c03fc4f05b1a914f33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dadcf68274e1468a62c7",
+    "url": "/static/css/main.414d4c9b.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dadcf68274e1468a62c7",
+    "url": "/static/js/main.af2ae0ca.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
